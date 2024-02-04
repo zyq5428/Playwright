bd6c790564021aa07bad65c0ef79e52f11cdc204
zyq5428 <594270026@qq.com>
2024-02-05 00:17:02 +0800

    feat: 实现分类小说的抓取
    1、抓取分类小说中今日更新的小说
    2、进入对应小说，手机小说信息

--- qzxs.py
+++ qzxs.py
@@ -12,15 +12,22 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(m
 
 # 定义MongoDB的连接字符串
 MONGO_CONNECTION_STRING = 'mongodb://localhost:27017'
-MONGO_DB_NAME = 'movies0202'
-MONGO_COLLECTION_NAME = 'movies0202'
+MONGO_DB_NAME = 'novel_0204'
+MONGO_COLLECTION_NAME = 'novel_0204'
 
 client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
 db = client[MONGO_DB_NAME]
 collection = db[MONGO_COLLECTION_NAME]
 
 BASE_URL = 'https://www.qzsc.cc/'
-XUANHUAN = 'xuanhuanxiaoshuo/'
+# cat_name = {
+#     'xuanhuanxiaoshuo/': '玄幻',
+#     'xiuzhenxiaoshuo/': '修真',
+#     'dushixiaoshuo/': '都市',
+#     }
+cat_name = {
+    'xuanhuanxiaoshuo/': '玄幻'
+    }
 
 # Setting
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
@@ -93,25 +100,21 @@ async def simulate_login(url):
 参数: context (浏览器上下文), url (爬取页面页码)
 返回值: page_obj.content() (页面代码源文档)
 '''
-async def scrape_index(context, page_id):
+async def scrape_category(context, url):
     async with sem_index:
-        index_url = f'{BASE_URL}page/{page_id}'
         page_obj = await context.new_page()
-        html = await scrape_api(page_obj, index_url)
+        html = await scrape_api(page_obj, url)
         await page_obj.close()
         return html
 
-def parse_index(html):
+def parse_category(html):
     doc = pq(html)
-    for item in doc('.el-card .name').items():
+    urls = []
+    for item in doc('ul li .s2 a').items():
         url = urljoin(BASE_URL, item.attr('href'))
         logging.info('Get detail url %s', url)
-        DETAIL_URL.append(url)
-
-async def process_index(context, page_id):
-    html = await scrape_index(context, page_id)
-    parse_index(html)
-    logging.info('第%d页抓取完毕', page_id)
+        urls.append(url)
+    return urls
 
 async def scrape_detail(context, url):
     async with sem_detail:
@@ -122,23 +125,29 @@ async def scrape_detail(context, url):
 
 def parse_detail(html):
     doc = pq(html)   # 将源代码初始化为PyQuery对象
-    cover = doc('img.cover').attr('src')
-    name = doc('a > h2').text()
-    categories = [item.text() for item in doc('.categories button span').items()]
-    published_at = doc('.info:contains(上映)').text()
-    published_at = re.search(r'\d{4}-\d{2}-\d{2}', published_at).group(0) \
-        if published_at and re.search(r'\d{4}-\d{2}-\d{2}', published_at) else None
-    drama = doc('.drama p').text()
-    score = doc('.score').text()
-    score = float(score) if score else None
-    return {
-        'cover': cover,
-        'name': name,
-        'categories': categories,
-        'published_at': published_at,
-        'drama': drama,
-        'score': score
-    }
+    # cover = doc('img.cover').attr('src')
+    name = doc('#info h1').text()
+    print(name)
+
+# def parse_detail(html):
+#     doc = pq(html)   # 将源代码初始化为PyQuery对象
+#     cover = doc('img.cover').attr('src')
+#     name = doc('a > h2').text()
+#     categories = [item.text() for item in doc('.categories button span').items()]
+#     published_at = doc('.info:contains(上映)').text()
+#     published_at = re.search(r'\d{4}-\d{2}-\d{2}', published_at).group(0) \
+#         if published_at and re.search(r'\d{4}-\d{2}-\d{2}', published_at) else None
+#     drama = doc('.drama p').text()
+#     score = doc('.score').text()
+#     score = float(score) if score else None
+#     return {
+#         'cover': cover,
+#         'name': name,
+#         'categories': categories,
+#         'published_at': published_at,
+#         'drama': drama,
+#         'score': score
+#     }
 
 '''
 程序作用: 保存数据到mongodb
@@ -157,8 +166,21 @@ async def save_data(data):
 
 async def process_detail(context, url):
     html = await scrape_detail(context, url)
-    data = parse_detail(html)
-    await save_data(data)
+    if html == None:
+        logging.info('The website cannot be accessed: %s', url)
+    else:
+        data = parse_detail(html)
+        # await save_data(data)
+
+
+async def process_category(context, category):
+    url = urljoin(BASE_URL, category)
+    html = await scrape_category(context, url)
+    # print(html)
+    updated_novel_urls = parse_category(html)
+    logging.info('%s 分类页抓取完毕', cat_name[category])
+    detail_scrape_task = [asyncio.create_task(process_detail(context, url)) for url in updated_novel_urls]
+    done, pending = await asyncio.wait(detail_scrape_task, timeout=None)
 
 async def main():
     # await simulate_login(BASE_URL)
@@ -166,34 +188,12 @@ async def main():
     async with async_playwright() as playwright:
         chromium = playwright.chromium
         browser = await chromium.launch(headless=False)
-        context = await browser.new_context(user_agent = USER_AGENT)
-        page = await context.new_page()
-        html = await scrape_api(page, BASE_URL)
-        print(html)
-        await page.close()
-        await context.close()
+        context_category = await browser.new_context(user_agent = USER_AGENT)
+        category_scrape_task = [asyncio.create_task(process_category(context_category, key)) 
+                             for key, value in cat_name.items()]
+        done, pending = await asyncio.wait(category_scrape_task, timeout=None)
+        await context_category.close()
         await browser.close()
-
-    # async with async_playwright() as playwright:
-    #     chromium = playwright.chromium
-    #     browser = await chromium.launch(headless=False)
-    #     context_index = await browser.new_context(storage_state=COOKIE_FILE)
-    #     index_scrape_task = [asyncio.create_task(process_index(context_index, page_id)) 
-    #                          for page_id in range(1, PAGE_NUM + 1)]
-    #     done, pending = await asyncio.wait(index_scrape_task, timeout=None)
-    #     print(DETAIL_URL)
-    #     await context_index.close()
-    #     await browser.close()
-
-    # async with async_playwright() as playwright:
-    #     chromium = playwright.chromium
-    #     browser = await chromium.launch(headless=False)
-    #     context_detail = await browser.new_context(storage_state=COOKIE_FILE)
-    #     detail_scrape_task = [asyncio.create_task(process_detail(context_detail, url)) 
-    #                           for url in DETAIL_URL]
-    #     done, pending = await asyncio.wait(detail_scrape_task, timeout=None)
-    #     await context_detail.close()
-    #     await browser.close()
 
 if __name__ == '__main__':
     start_time = time.time()
