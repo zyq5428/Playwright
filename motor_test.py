a8ecc2b278bd74f64a74eea097c184f59407922c
zyq5428 <594270026@qq.com>
2024-02-18 23:43:47 +0800

    feat: 完成单本小说的下载并保存

--- motor_test.py
+++ motor_test.py
@@ -14,6 +14,10 @@ client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
 db = client[MONGO_DB_NAME]
 collection = db[MONGO_COLLECTION_NAME]
 
+BOOK_NAME = '废土'
+BOOK_CHAPTER_COLLECTION_NAME = '章节链接'
+BOOK_TEXT_COLLECTION_NAME = '文本'
+
 async def do_find():
     async for document in collection.find({}):  # 查询所有文档
         name = document.get('name')
@@ -21,8 +25,21 @@ async def do_find():
         chapter_info = document.get('chapter_info')
         pprint(chapter_info)
 
+async def save_to_txt():
+    global BOOK_NAME
+
+    db = client[BOOK_NAME]
+    collection = db[BOOK_TEXT_COLLECTION_NAME]
+
+    cursor = collection.find({})
+    cursor.sort('index', 1)
+
+    with open(BOOK_NAME + ".txt", "w", encoding="utf-8") as file:
+        async for document in cursor:
+            file.write(document.get('content') + "\n\n")
+
 async def main():
-    await do_find()
+    await save_to_txt()
 
 if __name__ == '__main__':
     asyncio.run(main())
--- novel_download.py
+++ novel_download.py
@@ -9,7 +9,7 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(levelname)s: %(message)s')
 
 # 推荐文档：https://www.cnblogs.com/aduner/p/13532504.html
 # 定义MongoDB的连接字符串
@@ -94,108 +94,21 @@ async def simulate_login(url):
         except Exception as e:
             logging.error('error occurred while scraping %s', url, exc_info=True)
 
-'''
-程序作用: 爬取指定页码,返回页面代码源文档
-参数: context (浏览器上下文), url (爬取页面页码)
-返回值: page_obj.content() (页面代码源文档)
-'''
-async def scrape_category(context, url):
-    async with sem_index:
-        page_obj = await context.new_page()
-        html = await scrape_api(page_obj, url)
-        await page_obj.close()
-        return html
-
-def parse_category(html):
+def parse_chapter_list(html):
     doc = pq(html)
-    urls = []
-    for item in doc('ul li .s2 a').items():
-        url = urljoin(BASE_URL, item.attr('href'))
-        logging.info('Get detail url %s', url)
-        urls.append(url)
-    return urls
-
-async def scrape_detail(context, url):
-    async with sem_detail:
-        page_obj = await context.new_page()
-        html = await scrape_api(page_obj, url)
-        await page_obj.close()
-        return html
-
-def parse_detail(html):
-    doc = pq(html)   # 将源代码初始化为PyQuery对象
     try:
-        name = doc('#info h1').text()
-        author = doc('#info p').text()
-        author = re.search(r'者：(.*)', doc('#info p:contains(者：)').text()).group(1)
-        last_update = re.search(r'更新：(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})', doc('#info p:contains(更新：)').text()).group(1)
-        brief = doc('#intro p').text()
-        chapters = doc('.box_con #list a')
-        chapter_info = []
-        for chapter in chapters.items():
-            url = urljoin(BASE_URL, chapter.attr('href'))
-            title = chapter.text()
-            chapter_info.append({
-                'url': url,
-                'title': title
-            })
-        return {
-            'name': name,
-            'author': author,
-            'last_update': last_update,
-            'brief': brief,
-            'chapter_info': chapter_info
-        }
+        urls = []
+        for item in doc('.listpage').items():
+            option = item('option')
+            for item in option.items():
+                url = urljoin(BASE_URL, item.attr('value'))
+                logging.info('Get chapter_list url %s', url)
+                urls.append(url)
+            break
+        return urls
     except Exception as e:
         logging.error('Failed to parse the website', exc_info=True)
         return None
-
-'''
-程序作用: 保存数据到mongodb
-参数: data (数据)
-返回值: 无
-'''
-async def save_data(data):
-    # logging.info('Save data %s', data)
-    if data:
-        return await     collection.update_one({
-        'name': data.get('name')
-        }, {
-            '$set': data
-        }, upsert=True
-    )
-
-async def process_detail(context, url):
-    html = await scrape_detail(context, url)
-    if html == None:
-        logging.error('The website cannot be accessed: %s', url)
-        ACCESS_FAILED_URL.append(url)
-    else:
-        data = parse_detail(html)
-        if data:
-            await save_data(data)
-
-
-async def process_category(context, category):
-    url = urljoin(BASE_URL, category)
-    html = await scrape_category(context, url)
-    # print(html)
-    updated_novel_urls = parse_category(html)
-    logging.warning('%s 分类页抓取完毕, 需抓取 %d 本小说', cat_name[category], len(updated_novel_urls))
-    detail_scrape_task = [asyncio.create_task(process_detail(context, url)) for url in updated_novel_urls]
-    done, pending = await asyncio.wait(detail_scrape_task, timeout=None)
-
-def parse_chapter_list(html):
-    doc = pq(html)
-    urls = []
-    for item in doc('.listpage').items():
-        option = item('option')
-        for item in option.items():
-            url = urljoin(BASE_URL, item.attr('value'))
-            logging.info('Get chapter_list url %s', url)
-            urls.append(url)
-        break
-    return urls
 
 async def get_chapter_url():
     global BOOK_NAME
@@ -276,34 +189,38 @@ async def save_novel_text(data):
 
     if data:
         return await     collection.update_one({
-        'name': data.get('index')
+        'index': data.get('index')
         }, {
             '$set': data
         }, upsert=True
     )
 
-# https://deepinout.com/mongodb/mongodb-questions/233_mongodb_how_to_check_if_a_pymongo_cursor_has_query_results.html
 async def check_exist(index):
     global BOOK_NAME
     db = client[BOOK_NAME]
     collection = db[BOOK_TEXT_COLLECTION_NAME]
-    result = await collection.find_one({index: {'$exists': True}})
+    result = await collection.count_documents({'index': index}) > 0
+    return result
 
 async def get_chapter_text(context, index, url, sem):
-    async with sem:
-        page_obj = await context.new_page()
-        html = await scrape_api(page_obj, url)
-        if html == None:
-            logging.error('The website cannot be accessed: %s', url)
-            ACCESS_FAILED_URL.append({
-                'index': index,
-                'url': url
-            })
-        else:
-            data = parse_chapter_text(index, html)
-            if data:
-                await save_novel_text(data)
-        await page_obj.close()
+    if await check_exist(index):
+        logging.info('novel text is exist')
+        return
+    else:
+        async with sem:
+            page_obj = await context.new_page()
+            html = await scrape_api(page_obj, url)
+            if html == None:
+                logging.error('The website cannot be accessed: %s', url)
+                ACCESS_FAILED_URL.append({
+                    'index': index,
+                    'url': url
+                })
+            else:
+                data = parse_chapter_text(index, html)
+                if data:
+                    await save_novel_text(data)
+            await page_obj.close()
 
 async def get_chapter_info():
     global BOOK_NAME
@@ -315,6 +232,8 @@ async def get_chapter_info():
     sem_detail = asyncio.Semaphore(CONCURRENCY_DETAIL_VALUE)
     sem_retry = asyncio.Semaphore(CONCURRENCY_RETRY_VALUE)
 
+    retry_cnt = 0
+
     chapter_urls = []
     async for document in collection.find({}):  # 查询所有文档
         if document.get('name') == BOOK_NAME:
@@ -324,20 +243,36 @@ async def get_chapter_info():
         chromium = playwright.chromium
         browser = await chromium.launch(headless=False)
         context = await browser.new_context(user_agent = USER_AGENT)
-        detail_scrape_task = [asyncio.create_task(get_chapter_text(context, id, url, sem_detail)) for id, url in enumerate(chapter_urls)]
+        detail_scrape_task = [asyncio.create_task(get_chapter_text(context, id + 1, url, sem_detail)) for id, url in enumerate(chapter_urls)]
         done, pending = await asyncio.wait(detail_scrape_task, timeout=None)
         await context.close()
         await browser.close()
 
     while(ACCESS_FAILED_URL):
-        chromium = playwright.chromium
-        browser = await chromium.launch(headless=False)
-        context = await browser.new_context(user_agent = USER_AGENT)
-        retry_scrape_task = [asyncio.create_task(get_chapter_text(context, item.get('index'), item.get('url'), sem_retry)) for item in ACCESS_FAILED_URL]
-        ACCESS_FAILED_URL = []
-        done, pending = await asyncio.wait(retry_scrape_task, timeout=None)
-        await context.close()
-        await browser.close()
+        retry_cnt += 1
+        logging.warning('Start recrawling the information for the %s time', retry_cnt)
+        async with async_playwright() as playwright:
+            chromium = playwright.chromium
+            browser = await chromium.launch(headless=False)
+            context = await browser.new_context(user_agent = USER_AGENT)
+            retry_scrape_task = [asyncio.create_task(get_chapter_text(context, item.get('index'), item.get('url'), sem_retry)) for item in ACCESS_FAILED_URL]
+            ACCESS_FAILED_URL = []
+            done, pending = await asyncio.wait(retry_scrape_task, timeout=None)
+            await context.close()
+            await browser.close()
+
+async def save_to_txt():
+    global BOOK_NAME
+
+    db = client[BOOK_NAME]
+    collection = db[BOOK_TEXT_COLLECTION_NAME]
+
+    cursor = collection.find({})
+    cursor.sort('index', 1)
+
+    with open(BOOK_NAME + ".txt", "w", encoding="utf-8") as file:
+        async for document in cursor:
+            file.write(document.get('content') + "\n\n")
 
 async def main():
     # 获取小说章节链接并保存到mongodb
@@ -345,6 +280,9 @@ async def main():
 
     # 获取每个章节的内容并保存到mongodb
     await get_chapter_info()
+
+    # 将小说内容拼接成txt文件
+    await save_to_txt()
 
 
 
