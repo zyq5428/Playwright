20e5dbcac52f1fbcbba63bc58befa99356014650
zyq5428 <594270026@qq.com>
2024-02-21 00:47:23 +0800

    feat: 实现网页文字验证码的识别，但还存在bug
    1、需要去除确认部分的图片
    2、坐标的绝对位置计算出现了问题

--- level_4_captcha3_words.py
+++ level_4_captcha3_words.py
@@ -6,6 +6,7 @@ from hashlib import md5
 from PIL import Image
 import requests
 import asyncio
+import random
 import time
 import json
 import re
@@ -21,12 +22,15 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 USERNAME = 'admin'
 PASSWORD = 'admin'
 COOKIE_FILE = 'cookies.json'
+LOGGED_LABEL = '登录成功'
 
-CJ_USERNAME = '******'
-CJ_PASSWORD = '******'
-SOFT_ID = '******'
+# CJ_USERNAME = '******'
+# CJ_PASSWORD = '******'
+# SOFT_ID = '******'
 SCREENSHOT_FILE = 'website.png'
 CAPTCHA_FILE = 'captcha.png'
+GET_CAPTCHA_POSITION = False
+PIC_ID = ''
 
 PAGE_NUM = 10
 DETAIL_URL = []
@@ -102,6 +106,7 @@ def get_captcha_image(position):
     width = position.get('width')
     height = position.get('height')
     box = (x, y, x + width, y + height)
+    logging.info('Captcha box: %s', box)
     with Image.open(SCREENSHOT_FILE) as im:
         im_crop = im.crop(box)
         im_crop.save(CAPTCHA_FILE)
@@ -115,8 +120,10 @@ def get_relative_positions():
     with open(CAPTCHA_FILE, 'rb') as f:
         img = f.read()
         chaojiying = Chaojiying_Client(CJ_USERNAME, CJ_PASSWORD, SOFT_ID)
-        cjy_response = chaojiying.PostPic(im, 9004)
+        cjy_response = chaojiying.PostPic(img, 9004)
         if cjy_response['err_no'] == 0:
+            GET_CAPTCHA_POSITION = True
+            PIC_ID = cjy_response['pic_id']
             pic_str = cjy_response['pic_str']
             positions = re.split(r'\|', pic_str)
             pos = []
@@ -127,7 +134,14 @@ def get_relative_positions():
                     'x': x,
                     'y': y,
                 })
+            logging.info('Get captcha positions: %s', pos)
             return pos
+        
+def report_error_id():
+    chaojiying = Chaojiying_Client(CJ_USERNAME, CJ_PASSWORD, SOFT_ID)
+    logging.info('Report error picture id: %s', PIC_ID)
+    cjy_response = chaojiying.ReportError(PIC_ID)
+    logging.info('Report error response: %s', cjy_response)
 
 '''
 程序作用: 获得验证码在网页中的位置
@@ -141,6 +155,10 @@ def get_absolute_positions(base_pos, rel_pos):
         position['x'] = x + position.get('x')
         position['y'] = x + position.get('y')
     return rel_pos
+
+async def sleep_ms_rand(lower, upper):
+    ms = random.randint(lower, upper)
+    await asyncio.sleep(ms / 1000)
 
 '''
 程序作用: 完成网站模拟登录,并保存cookies
@@ -163,15 +181,34 @@ async def simulate_login(url):
             await page.wait_for_load_state("networkidle")
             await page.locator('.geetest_panel_next').wait_for()
             await page.screenshot(path = SCREENSHOT_FILE)
-            captcha_position = await page.locator('.geetest_panel_next').bounding_box()
+            captcha_position = await page.locator('.geetest_widget').bounding_box()
             get_captcha_image(captcha_position)
             relative_positions = get_relative_positions()
             absolute_positions = get_absolute_positions(captcha_position, relative_positions)
+            for absolute_positon in absolute_positions:
+                logging.info('Click %s', absolute_positon)
+                await page.mouse.click(absolute_positon.get('x'), absolute_positon.get('y'))
+                await sleep_ms_rand(200, 500)
+            commit_tip = page.locator('.geetest_commit_tip')
+            await commit_tip.wait_for()
+            await commit_tip.click()
+            await page.wait_for_load_state("networkidle")
+            username = page.get_by_text(LOGGED_LABEL)
+            await username.wait_for()
+            # In order to obtain the token of JWT
+            await page.reload()
+            await page.wait_for_load_state("networkidle")
+            # save storage_state to file
+            storage = await context.storage_state(path=COOKIE_FILE)
+            logging.info('Cookies is saved to %s: \n %s', COOKIE_FILE, storage)
+            # await page.screenshot(path = SCREENSHOT_FILE)
             await page.close()
             await context.close()
             await browser.close()
         except Exception as e:
             logging.error('error occurred while scraping %s', url, exc_info=True)
+            if GET_CAPTCHA_POSITION == True: 
+                report_error_id()
 
 
 async def main():
